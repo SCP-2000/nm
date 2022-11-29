62eef912cc515c976d5653dc088ac2fb27bb0aa4
Nick Cao <nickcao@nichi.co>
2022-11-29 16:09:38 +0800

    expose more route attributes

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use axum::{
     Json, Router,
 };
 use futures::{stream::TryStreamExt, StreamExt, TryStream};
+use rtnetlink::packet::{nlas::route::Nla, AF_INET, AF_INET6};
 use rtnetlink::{new_connection, packet::RouteMessage, Handle, IpVersion};
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Default)]
 struct Route {
     pub address_family: u8,
     pub destination_prefix_length: u8,
@@ -19,20 +20,60 @@ struct Route {
     pub protocol: u8,
     pub scope: u8,
     pub kind: u8,
+    // TODO: flags
+    pub destination: Option<IpAddr>,
+    pub gateway: Option<IpAddr>,
+    pub prefsrc: Option<IpAddr>,
+    pub oif: Option<u32>,
+}
+
+fn address(family: u8, addr: Vec<u8>) -> IpAddr {
+    match family as u16 {
+        AF_INET => {
+            let mut buf = [0u8; 4];
+            buf.copy_from_slice(&addr);
+            IpAddr::V4(Ipv4Addr::from(buf))
+        }
+        AF_INET6 => {
+            let mut buf = [0u8; 16];
+            buf.copy_from_slice(&addr);
+            IpAddr::V6(Ipv6Addr::from(buf))
+        }
+        _ => unreachable!(),
+    }
 }
 
 impl From<RouteMessage> for Route {
-    fn from(route: RouteMessage) -> Self {
-        Self {
-            address_family: route.header.address_family,
-            destination_prefix_length: route.header.destination_prefix_length,
-            source_prefix_length: route.header.source_prefix_length,
-            tos: route.header.tos,
-            table: route.header.table,
-            protocol: route.header.protocol,
-            scope: route.header.scope,
-            kind: route.header.kind,
+    fn from(msg: RouteMessage) -> Self {
+        let mut route = Self {
+            address_family: msg.header.address_family,
+            destination_prefix_length: msg.header.destination_prefix_length,
+            source_prefix_length: msg.header.source_prefix_length,
+            tos: msg.header.tos,
+            table: msg.header.table,
+            protocol: msg.header.protocol,
+            scope: msg.header.scope,
+            kind: msg.header.kind,
+            ..Default::default()
+        };
+        for nla in msg.nlas {
+            match nla {
+                Nla::Destination(dst) => {
+                    route.destination = Some(address(msg.header.address_family, dst));
+                }
+                Nla::Gateway(gateway) => {
+                    route.gateway = Some(address(msg.header.address_family, gateway));
+                }
+                Nla::PrefSource(prefsrc) => {
+                    route.gateway = Some(address(msg.header.address_family, prefsrc));
+                }
+                Nla::Oif(oif) => {
+                    route.oif = Some(oif);
+                }
+                nla => log::debug!("ignored unsupported nla: {:?}", nla),
+            }
         }
+        route
     }
 }
 
