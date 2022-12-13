83730595243c1e098bbb0f6245e9a704ee88682b
Nick Cao <nickcao@nichi.co>
2022-12-13 12:48:21 +0800

    rework link handling

--- src/link.rs
+++ src/link.rs
@@ -1,41 +1,60 @@
+use crate::Error;
 use axum::{
     extract::{self, Path},
     Extension, Json,
 };
 use futures::stream::TryStreamExt;
-use rtnetlink::packet::nlas::link::Nla as LinkNla;
-
+use rtnetlink::packet::nlas::link::Nla;
 use rtnetlink::packet::LinkMessage;
-
 use rtnetlink::Handle;
-
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Default, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Link {
-    pub interface_family: u8,
+    pub family: u8,
     pub index: u32,
-    pub link_layer_type: u16,
+    pub linklayer: u16,
     pub flags: u32,
 
     pub ifname: Option<String>,
     pub mtu: Option<u32>,
 }
 
+fn push_nlas(link: &Link, nlas: &mut Vec<Nla>) {
+    if let Some(ifname) = &link.ifname {
+        nlas.push(Nla::IfName(ifname.to_string()));
+    }
+    if let Some(mtu) = link.mtu {
+        nlas.push(Nla::Mtu(mtu));
+    }
+}
+
+impl From<Link> for LinkMessage {
+    fn from(link: Link) -> Self {
+        let mut msg = Self::default();
+        msg.header.interface_family = link.family;
+        msg.header.index = link.index;
+        msg.header.link_layer_type = link.linklayer;
+        msg.header.flags = link.flags;
+        push_nlas(&link, &mut msg.nlas);
+        msg
+    }
+}
+
 impl From<LinkMessage> for Link {
     fn from(msg: LinkMessage) -> Self {
         let mut link = Self {
-            interface_family: msg.header.interface_family,
+            family: msg.header.interface_family,
             index: msg.header.index,
-            link_layer_type: msg.header.link_layer_type,
+            linklayer: msg.header.link_layer_type,
             flags: msg.header.flags,
             ..Default::default()
         };
         for nla in msg.nlas {
             match nla {
-                LinkNla::IfName(ifname) => link.ifname = Some(ifname),
-                LinkNla::Mtu(mtu) => link.mtu = Some(mtu),
-                nla => log::debug!("ignored unsupported link nla: {:?}", nla),
+                Nla::IfName(ifname) => link.ifname = Some(ifname),
+                Nla::Mtu(mtu) => link.mtu = Some(mtu),
+                _ => continue,
             }
         }
         link
@@ -58,14 +77,13 @@ pub async fn change(
     Json(payload)
 }
 
-pub async fn get(Extension(handle): Extension<Handle>) -> Json<Vec<Link>> {
+pub async fn get(Extension(handle): Extension<Handle>) -> Result<Json<Vec<Link>>, Error> {
     let links: Vec<Link> = handle
         .link()
         .get()
         .execute()
         .map_ok(Link::from)
         .try_collect()
-        .await
-        .unwrap();
-    Json(links)
+        .await?;
+    Ok(Json(links))
 }
