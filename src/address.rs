8a36f022fb1a3865fb3df23d86df63bd724fc1ae
Nick Cao <nickcao@nichi.co>
2022-12-13 12:23:26 +0800

    use octets_to_addr in address

--- src/address.rs
+++ src/address.rs
@@ -1,17 +1,15 @@
+use crate::util::octets_to_addr;
 use axum::{Extension, Json};
 use futures::stream::TryStreamExt;
-
 use rtnetlink::packet::AddressMessage;
-use rtnetlink::packet::{AF_INET, AF_INET6};
 use rtnetlink::{packet::nlas::address::Nla as AddrNla, Handle};
-
 use serde::Serialize;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::IpAddr;
 
 #[derive(Debug, Serialize, Default)]
 pub struct Address {
     pub family: u8,
-    pub prefix_len: u8,
+    pub plen: u8,
     pub flags: u8,
     pub scope: u8,
     pub index: u32,
@@ -22,27 +20,11 @@ pub struct Address {
     pub broadcast: Option<IpAddr>,
 }
 
-fn to_address(family: u8, addr: Vec<u8>) -> IpAddr {
-    match family as u16 {
-        AF_INET => {
-            let mut buf = [0u8; 4];
-            buf.copy_from_slice(&addr);
-            IpAddr::V4(Ipv4Addr::from(buf))
-        }
-        AF_INET6 => {
-            let mut buf = [0u8; 16];
-            buf.copy_from_slice(&addr);
-            IpAddr::V6(Ipv6Addr::from(buf))
-        }
-        _ => unreachable!(),
-    }
-}
-
 impl From<AddressMessage> for Address {
     fn from(msg: AddressMessage) -> Self {
         let mut address = Self {
             family: msg.header.family,
-            prefix_len: msg.header.prefix_len,
+            plen: msg.header.prefix_len,
             flags: msg.header.flags,
             scope: msg.header.scope,
             index: msg.header.index,
@@ -50,13 +32,11 @@ impl From<AddressMessage> for Address {
         };
         for nla in msg.nlas {
             match nla {
-                AddrNla::Address(addr) => {
-                    address.address = Some(to_address(msg.header.family, addr))
-                }
-                AddrNla::Local(local) => address.local = Some(to_address(msg.header.family, local)),
+                AddrNla::Address(addr) => address.address = Some(octets_to_addr(&addr)),
+                AddrNla::Local(local) => address.local = Some(octets_to_addr(&local)),
                 AddrNla::Label(label) => address.label = Some(label),
                 AddrNla::Broadcast(broadcast) => {
-                    address.broadcast = Some(to_address(msg.header.family, broadcast))
+                    address.broadcast = Some(octets_to_addr(&broadcast))
                 }
                 nla => log::debug!("ignored unsupported address nla: {:?}", nla),
             }
